22136845d3b1d8891ec6db805aac6699f890ba76
Moveqqq <moveq001115@gmail.com>
2025-05-07 03:35:36 +0900

    [fix] User/Student 스키마/더미 데이터 수정

--- examples/StudentEx.js
+++ examples/StudentEx.js
@@ -11,8 +11,6 @@ mongoose
       name: "김민준",
       gender: "남자",
       registration_number: "031214-3124546",
-      address: "인천광역시 연수구 연수동",
-      phone: "010-3245-6574",
       class_id: "68176e5d84223682274493b7",
       class_history: null,
     });
@@ -22,8 +20,6 @@ mongoose
       name: "이서윤",
       gender: "여자",
       registration_number: "031156-2545678",
-      address: "인천광역시 연수구 송도동",
-      phone: "010-3226-9870",
       class_id: "68176e5d84223682274493b7",
       class_history: null,
     });
@@ -33,8 +29,6 @@ mongoose
       name: "박지후",
       gender: "남자",
       registration_number: "030512-3156790",
-      address: "인천광역시 연수구 동춘동동",
-      phone: "010-1235-6374",
       class_id: "68176e5d84223682274493b7",
       class_history: null,
     });
@@ -44,8 +38,6 @@ mongoose
       name: "최하은",
       gender: "여자",
       registration_number: "030312-2453241",
-      address: "인천광역시 연수구 옥련동",
-      phone: "010-23454-6504",
       class_id: "68176e5d84223682274493b7",
       class_history: null,
     });
@@ -55,8 +47,6 @@ mongoose
       name: "정도윤",
       gender: "남자",
       registration_number: "031204-3543674",
-      address: "서울특별시 강남구 논현동",
-      phone: "010-1984-2435",
       class_id: "68176e5d84223682274493b7",
       class_history: null,
     });
@@ -66,8 +56,6 @@ mongoose
       name: "한예린",
       gender: "여자",
       registration_number: "030214-2124246",
-      address: "경기도 시흥시",
-      phone: "010-3047-9486",
       class_id: "68176e5d84223682274493b7",
       class_history: null,
     });
@@ -77,8 +65,6 @@ mongoose
       name: "오준서",
       gender: "남자",
       registration_number: "030121-3645198",
-      address: "인천광역시 남동구 가산동",
-      phone: "010-9765-3671",
       class_id: "68176e5d84223682274493b7",
       class_history: null,
     });
@@ -88,8 +74,6 @@ mongoose
       name: "윤지아",
       gender: "여자",
       registration_number: "031115-2453423",
-      address: "경기도 수원시 성화동",
-      phone: "010--3452-8761",
       class_id: "68176e5d84223682274493b7",
       class_history: null,
     });
@@ -99,8 +83,6 @@ mongoose
       name: "강도현",
       gender: "남자",
       registration_number: "030912-3456981",
-      address: "인천광역시 서구 서울동",
-      phone: "010-1187-9812",
       class_id: "68176e5d84223682274493b7",
       class_history: null,
     });
@@ -110,8 +92,6 @@ mongoose
       name: "조하율",
       gender: "여자",
       registration_number: "030812-2314981",
-      address: "인천광역시 미추홀구 미추홀동",
-      phone: "010-3212-1231",
       class_id: "68176e5d84223682274493b7",
       class_history: null,
     });
