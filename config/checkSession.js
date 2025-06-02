d1d6386b740fe9c007a4c3a6e77ca33bf949bddd
Moveqqq <moveq001115@gmail.com>
2025-06-03 01:15:54 +0900

    [feat] csrfToken 검사

--- config/checkSession.js
+++ config/checkSession.js
@@ -1,9 +1,13 @@
 const publicPaths = [
   "/users/sign-up",
   "/users/kakao/login",
+  "/users/kakao/callback",
   "/users/csrf-Token",
   "/users/check-Id",
 ];
+
+const Tokens = require("csrf");
+const tokens = new Tokens();
 
 module.exports = function checkSession(req, res, next) {
   // 예외 경로는 검사 없이 통과
@@ -21,6 +25,21 @@ module.exports = function checkSession(req, res, next) {
   if (req.session.cookie && req.session.cookie.expires) {
     if (new Date(req.session.cookie.expires) < new Date()) {
       return res.status(400).json({ message: "세션이 만료됨" });
+    }
+  }
+
+  const method = req.method.toUpperCase();
+  if (["POST", "PUT", "PATCH", "DELETE"].includes(method)) {
+    const csrfToken = req.headers["x-csrf-token"];
+
+    if (!csrfToken) {
+      return res.status(403).json({ message: "CSRF 토큰이 없습니다." });
+    }
+
+    if (!tokens.verify(req.session.csrfSecret, csrfToken)) {
+      return res
+        .status(403)
+        .json({ message: "CSRF 토큰이 유효하지 않습니다." });
     }
   }
 
